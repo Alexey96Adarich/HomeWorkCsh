06e633c49c86b9271ca958bac4a9f1813c1d4bfa
AlexeyAdarich <alexey96adarich@mail.ru>
2021-05-23 16:48:26 +0900

    Lesson7

---
+++ Lesson7/Ex1/Ex1/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ex1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Random r;    // загадываем число
+        int n;
+        Stack<string> history = new Stack<string>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            btnCommand1.Content = "+1";     // кнопка. параметры
+            btnCommand2.Content = "x2";     // кнопка. параметры
+            btnReset.Content = "Сброс";      /// кнопка
+            lblNumber.Content = "0";       //  окно вывода инф. параметры
+            lblCount.Content = "0";        //  окно вывода нажатий. параметры
+            this.Title = "Удвоитель";      //название
+            r = new Random();
+        }
+        int count;
+        private void btnCommand1_Click(object sender, RoutedEventArgs e)
+        {
+            var t = Convert.ToInt32(lblNumber.Content);
+            history.Push(t.ToString());
+            if (t != n)
+            {
+                t += 1;
+                lblNumber.Content = t.ToString();     //кнопка   +1
+                count++;
+                lblCount.Content = count;
+            }
+            if (t == n) MessageBox.Show($"ура");
+        }
+
+        private void btnCommand2_Click(object sender, RoutedEventArgs e)
+        {
+            var t = Convert.ToInt32(lblNumber.Content);
+            history.Push(t.ToString());
+            if (t != n)
+            {
+                t *= 2;
+                lblNumber.Content = t.ToString();     //кнопка   *1
+                count++;
+                lblCount.Content = count;
+            }
+            if (t == n) MessageBox.Show($"ура");
+        }
+
+        private void btnReset_Click(object sender, RoutedEventArgs e)
+        {
+            NewMethod();
+        }
+        private void NewMethod()
+        {
+            lblNumber.Content = "1";
+            count = 0;
+            lblCount.Content = count;
+            n = r.Next(20, 40);
+            MessageBox.Show($"Угадай {n}");
+        }
+        private void newGameClick(object sender, RoutedEventArgs e)   //добавили меню
+        {
+            NewMethod();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            lblNumber.Content = history.Pop();
+        }
+    }
+}
---
+++ Lesson7/Ex2/Ex2/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ex2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Random r;    // загадываем число
+        int n;     // сгенерированное
+        int a;      // введеное
+        int count = 6;
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Title = "Угадай число";      //название проекта
+            r = new Random();
+        }
+
+        private void guy_Click(object sender, RoutedEventArgs e)
+        {
+            n = r.Next(1, 100);
+            MessageBox.Show($"Угадай загаданное число от 1 до 100. У вас " + count + " попытки");       
+        }
+
+        private void start_Click(object sender, RoutedEventArgs e)
+        {
+            a = Convert.ToInt32(play.Text);   // вызываем текст
+            if (a == n)
+            {
+                MessageBox.Show($"Ура! Компьютер загадал число {n}");
+            }
+            else if (n > a)
+            {
+                MessageBox.Show($"Загаданное число больше, попытка № " + (count - 1) /*(count.Length - 1)*/);
+                count--;
+            }
+            else if (n < a)
+            {
+                MessageBox.Show($"Загаданное число меньше, попытка № " + (count - 1));
+                count--;
+            }
+            if (count == 0)
+            {
+                MessageBox.Show($"Увы, вы не отгадали загаданное число. Это было число " + n);
+                Close();
+            }
+        }
+    }
+}
